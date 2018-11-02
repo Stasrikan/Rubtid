446f3a794e5a67410aeb52714dbbbb57bf3e4ee0
Stasrikan <44445171+Stasrikan@users.noreply.github.com>
2018-11-02 16:52:41 +0300

    Update Tabloid.cs

--- mobs/Tabloid.cs
+++ mobs/Tabloid.cs
@@ -4,5 +4,33 @@ using Terraria.ModLoader;
 
 using Microsoft.Xna.Framework;
 
-using Tremor.Items;
-using Tremor.Items.Wolf;
+using Rubtid.Items;
+namespace Rubtid.mobs
+{
+  public class Tabloid :  ModNPCs
+  {
+    public override void SetStaticDefauls()
+    {
+      DisplayName.SetDefault("Tabloid");
+    }
+    public override void SetDefaults()
+    {
+       npc.lifeMax = 50;
+       npc.damage = 10;
+       npc.defence = 2;
+       npc.width = 64;
+       npc.height = 64;
+       npc.aiStyle = 26;
+       npc.hitSound =SoundID.NPCHit6;
+       npc.DeathSound = SoundID.NPCDeath5;
+       }
+       public override float CanSpawn(NPCSpawnInfo spawnInfo)
+       {
+        return spawnInfo.spawnTileY < Main.rockLaye ? 0.08f : 0f;
+        }
+        
+        
+    
+ 
+     
+       
